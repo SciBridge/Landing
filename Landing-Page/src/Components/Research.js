a5fcd5913c429f7d00340d62bd5220f251c68819
SciBridge <scibridgelabs@gmail.com>
2024-07-31 22:12:23 +0530

    purpose

--- Landing-Page/src/Components/Research.js
+++ Landing-Page/src/Components/Research.js
@@ -5,7 +5,7 @@ const Research = () => {
   return (
     <div className="work-section-wrapper">
       <div className="work-section-top">
-        <h1 className="primary-heading">How We Research?</h1>
+        <h1 className="primary-heading">Our Purpose</h1>
         <p className="primary-text">
         The primary impetus for this project was to simplify the tedious process of preparing results for research paper writing.
          Frequently, tasks such as calculating results or creating graphs from data involve writing code in various programming languages, 
